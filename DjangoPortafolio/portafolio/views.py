2d433483034dbcbc3962d339ebdc993a0d615f95
José María  González <zljosewark1a@gmail.com>
2023-02-15 15:09:18 -0500

    last commit

---
+++ DjangoPortafolio/portafolio/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import Project
+
+def home(request):
+    projects=Project.objects.all()
+
+    return render(request, 'home.html', {'projects': projects})

c6a0dec413d8c8b70e492d0c25fa74edb943a162
José María  González <zljosewark1a@gmail.com>
2023-02-17 09:57:39 -0500

    last commit eight

--- DjangoPortafolio/portafolio/tests.py
+++ DjangoPortafolio/portafolio/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from .models import Project
+from django.core.files.uploadedfile import SimpleUploadedFile
 
-# Create your tests here.
+#Test para comprobar que se puede crear una instancia del modelo "project" correctamente
+class ProjectModelTest(TestCase):
+    def test_create_project(self):
+        project1 = Project(title="Proyecto 1", description="Descripción del proyecto 1")
+        project1.save()
+        self.assertEqual(project1.title, "Proyecto 1")
+        self.assertEqual(project1.description, "Descripción del proyecto 1")
+
+
+#Test para comprobar que se puede subir una imagen correctamente
+class ProjectModelTest(TestCase):
+    def test_upload_image(self):
+        image = SimpleUploadedFile("test_image.jpg", b"random_image_content", content_type="image/jpeg")
+        project1 = Project(title="Proyecto 1", description="Descripción del proyecto 1", image=image)
+        project1.save()
+        self.assertEqual(project1.image.name, "portafolio/images/test_image.jpg")
+
+
+#Test para comprobar que se puede asignar una URL correctamente
+class ProjectModelTest(TestCase):
+    def test_assign_url(self):
+        project1 = Project(title="Proyecto 1", description="Descripción del proyecto 1", url="https://www.google.com")
+        project1.save()
+        self.assertEqual(project1.url, "https://www.google.com")
+           
+
+#Test para comprobar que se puede actualizar una instancia del modelo "project" correctamentE
+class ProjectModelTest(TestCase):
+    def test_update_project(self):
+        project1 = Project(title="Proyecto 1", description="Descripción del proyecto 1")
+        project1.save()
+        project1.title = "Proyecto 2"
+        project1.description = "Descripción del proyecto 2"
+        project1.save()
+        updated_project = Project.objects.get(id=project1.id)
+        self.assertEqual(updated_project.title, "Proyecto 2")
+        self.assertEqual(updated_project.description, "Descripción del proyecto 2")
+
--- README.md
+++ README.md
@@ -3,3 +3,21 @@
 <p>
 <img style="text-align:center"; src="https://i.postimg.cc/sXpwzxZ6/mi-avatar.png" >
 <p>
+
+This project was carried out with the Django and python framework, it consisted in making our portfolio which will be our letter of introduction when facing the work environment, based on the knowledge that we have acquired in the course of our learning process, supporting us in different libraries and videos that we find on the web putting ourselves to the test and surpassing ourselves.
+
+<h1>Steps to follow</h1>
+
+<ul>
+   <li>The design was created in figma</li>
+   <li>A new project was made with Django</li>
+   <li>The respective apps were created, which in this case were the 'app portfolio' and the 'app blog'</li>
+   <li>Modifications were made in the different styles</li>
+   <li>The respective test was carried out</li>
+   <li>Finished the project with its respective deploy, it was deployed in pythonanywhere</li>
+</ul>
+
+
+
+<h1>Author</h1>
+Jose Maria Gonzalez Machin
